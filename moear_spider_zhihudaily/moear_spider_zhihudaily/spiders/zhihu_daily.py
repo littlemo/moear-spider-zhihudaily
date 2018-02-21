4e2fd2792f29be518f19557f816144201dd9fb31
Moore.Huang <moore@moorehy.com>
2018-02-21 23:05:03 +0800

    F: 在构造方法中实现start_urls填充

--- moear_spider_zhihudaily/moear_spider_zhihudaily/spiders/zhihu_daily.py
+++ moear_spider_zhihudaily/moear_spider_zhihudaily/spiders/zhihu_daily.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import datetime
 
 
 class ZhihuDailySpider(scrapy.Spider):
@@ -19,6 +20,32 @@ class ZhihuDailySpider(scrapy.Spider):
     description = "每天三次，每次七分钟。在中国，资讯类移动应用的人均阅读时长是 5 分钟，而在知乎日报，这个数字是 21"
 
     allowed_domains = ['zhihu.com']
+
+    def __init__(self, date=None, *args, **kwargs):
+        """
+        知乎日报爬虫类，用于爬取&解析知乎日报页面&相关协议
+        :param date:  爬取日期，命令行参数，默认为空，即爬取当日最新，内容格式：yyyymmdd
+        """
+        super(ZhihuDailySpider, self).__init__(*args, **kwargs)
+
+        self.start_urls = ['http://news-at.zhihu.com/api/4/news/latest']
+
+        # 此处由于知乎日报的协议为爬取指定日期的前一天
+        # 故需要将Spider接受的date日期+1天作为爬取参数
+        if date is not None:
+            self.logger.info('指定爬取参数：date={}'.format(date))
+            try:
+                spider_date = datetime.datetime.strptime(date, '%Y%m%d')
+                spider_date += datetime.timedelta(days=1)
+                spider_date_str = spider_date.strftime('%Y%m%d')
+                self.logger.info(
+                    '格式化后的知乎爬取日期参数：{}'.format(spider_date_str))
+                self.start_urls = [
+                    'http://news.at.zhihu.com/api/4/news/before/{}'.format(
+                        spider_date_str)]
+            except ValueError:
+                self.logger.error('指定的爬取日期错误(yyymmdd)：{}'.format(date))
+                self.start_urls = []
 
     def parse(self, response):
         pass
