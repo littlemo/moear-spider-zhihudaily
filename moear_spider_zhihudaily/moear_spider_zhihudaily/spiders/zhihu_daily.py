96718fb3e79285cd1b9af2fcc0db7cc96f6303ad
Moore.Huang <moore@moorehy.com>
2018-02-21 22:48:17 +0800

    A: 根据模板创建zhihu_daily爬虫

---
+++ moear_spider_zhihudaily/moear_spider_zhihudaily/spiders/zhihu_daily.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ZhihuDailySpider(scrapy.Spider):
+    name = 'zhihu_daily'
+    allowed_domains = ['zhihu.com']
+    start_urls = ['http://zhihu.com/']
+
+    def parse(self, response):
+        pass
