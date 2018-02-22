49ced99920fb44c9797e0083f91b9e83c395128d
Moore.Huang <moore@moorehy.com>
2018-02-22 16:47:33 +0800

    M: 设置为不遵循网站的robotstxt规则

--- moear_spider_zhihudaily/moear_spider_zhihudaily/settings.py
+++ moear_spider_zhihudaily/moear_spider_zhihudaily/settings.py
@@ -41,7 +41,7 @@ USER_AGENTS = [
 ]
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
