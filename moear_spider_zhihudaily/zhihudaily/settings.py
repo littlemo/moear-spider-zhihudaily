688db8e44341d59f437c8252af798dfe0c2473ae
Moore.Huang <moore@moorehy.com>
2018-03-01 21:34:19 +0800

    F: 修改包路径结构，缩短冗余的包名，同时修改setting中的相关配置参数

--- moear_spider_zhihudaily/zhihudaily/settings.py
+++ moear_spider_zhihudaily/zhihudaily/settings.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Scrapy settings for moear_spider_zhihudaily project
+# Scrapy settings for zhihudaily project
 #
 # For simplicity, this file contains only settings considered important or
 # commonly used. You can find more settings consulting the documentation:
@@ -9,13 +9,13 @@
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'moear_spider_zhihudaily'
+BOT_NAME = 'zhihudaily'
 
-SPIDER_MODULES = ['moear_spider_zhihudaily.spiders']
-NEWSPIDER_MODULE = 'moear_spider_zhihudaily.spiders'
+SPIDER_MODULES = ['moear_spider_zhihudaily.zhihudaily.spiders']
+NEWSPIDER_MODULE = 'moear_spider_zhihudaily.zhihudaily.spiders'
 
 DOWNLOADER_MIDDLEWARES = {
-    'moear_spider_zhihudaily.middlewares.RandomUserAgent': 1,
+    'moear_spider_zhihudaily.zhihudaily.middlewares.RandomUserAgent': 1,
 }
 
 
@@ -72,13 +72,13 @@ COOKIES_ENABLED = False
 # Enable or disable spider middlewares
 # See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
 #SPIDER_MIDDLEWARES = {
-#    'moear_spider_zhihudaily.middlewares.MoearSpiderZhihudailySpiderMiddleware': 543,
+#    'zhihudaily.middlewares.MoearSpiderZhihudailySpiderMiddleware': 543,
 #}
 
 # Enable or disable downloader middlewares
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #DOWNLOADER_MIDDLEWARES = {
-#    'moear_spider_zhihudaily.middlewares.MoearSpiderZhihudailyDownloaderMiddleware': 543,
+#    'zhihudaily.middlewares.MoearSpiderZhihudailyDownloaderMiddleware': 543,
 #}
 
 # Enable or disable extensions
@@ -90,7 +90,7 @@ COOKIES_ENABLED = False
 # Configure item pipelines
 # See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 #ITEM_PIPELINES = {
-#    'moear_spider_zhihudaily.pipelines.MoearSpiderZhihudailyPipeline': 300,
+#    'zhihudaily.pipelines.MoearSpiderZhihudailyPipeline': 300,
 #}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
