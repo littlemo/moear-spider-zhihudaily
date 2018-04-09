6cde0db1076c75981e25c1a6f31f5e0aabd490b6
Moore.Huang <moore@moorehy.com>
2018-04-09 10:16:58 +0800

    M: 根据知乎日报App中的官方分类标题，将'头条'修改为'热文'

--- moear_spider_zhihudaily/entry.py
+++ moear_spider_zhihudaily/entry.py
@@ -137,7 +137,7 @@ class ZhihuDaily(base.SpiderBase):
                 normal_list.append(item)
 
         if hot_list:
-            sections.setdefault('头条', hot_list)
+            sections.setdefault('热文', hot_list)
         if normal_list:
             sections.setdefault('日报', normal_list)
         return sections
